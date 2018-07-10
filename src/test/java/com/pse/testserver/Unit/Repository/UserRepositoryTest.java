81b562426464a1fae1770324c70c04a5fb33be10
utkuerol <utkuerol97@gmail.com>
2018-07-10 18:48:07 +0200

    experimenting unit tests

---
+++ src/test/java/com/pse/testserver/Unit/Repository/UserRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.pse.testserver.Unit.Repository;
+
+
+import com.pse.testserver.AppConfig;
+
+import com.pse.testserver.entities.User;
+import com.pse.testserver.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
+@Transactional
+@Rollback
+public class UserRepositoryTest {
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    User user1;
+    User user2;
+    User user3;
+
+    @Before
+    public void setUp() {
+        user1 = new User();
+        user2 = new User();
+        user3 = new User();
+        user1.setName("true");
+        user2.setName("truee");
+        user3.setName("false");
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+    }
+
+    @Test
+    public void findByName() {
+        List<User> foundUsersByName = userRepository.findByName("true");
+        List<User> trueFoundUsersByName = new LinkedList<>();
+        trueFoundUsersByName.add(user1);
+        trueFoundUsersByName.add(user2);
+        trueFoundUsersByName.add(user3);
+
+        assertTrue(foundUsersByName.equals(trueFoundUsersByName));
+    }
+
+
+}
