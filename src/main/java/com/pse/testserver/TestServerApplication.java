ac3090c377ef61898a168c5b27c19fd6c1dcbdcc
utkuerol <utkuerol97@gmail.com>
2018-08-10 00:46:07 +0200

    updated datasource

--- src/main/java/com/pse/testserver/TestServerApplication.java
+++ src/main/java/com/pse/testserver/TestServerApplication.java
@@ -3,6 +3,8 @@ package com.pse.testserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com.pse.testserver"})
 @SpringBootApplication
 @EnableAutoConfiguration
-public class TestServerApplication {
+public class TestServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestServerApplication.class, args);
 	}
+
 }
